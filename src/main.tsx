4bbb400047f293c458b7fb932b944724956097fe
Ivan Sarac <ivan.sarac@roomhero.de>
2025-04-20 14:31:59 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Typography } from '@mui/material';
+
+createRoot(document.getElementById('root')).render(<Typography>Park.me</Typography>);
